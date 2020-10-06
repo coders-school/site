ccc16a4c7b0ba2fd3a2e34ff1dd1b4f74cf01219
Łukasz Ziobroń <lukasz@coders.school>
2020-10-06 09:20:22 +0200

    Certificates updated (generated as image)

--- edu/certificates/templates/template_12/certificate.php
+++ edu/certificates/templates/template_12/certificate.php
@@ -4,56 +4,45 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>Coders School certificate</title>
     <style type="text/css"><?php $this->pdf_style(); ?></style>
+    <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&family=VT323&display=swap" rel="stylesheet">
 </head>
 <body>
 
 <div class="certificate-wrap">
 
     <div class="certificate-content">
-		<?php
+        <?php
 		$hour_text = '';
 		$min_text = '';
-		if ($durationHours){
-			if ($durationHours > 1){
-				$hour_text = $durationHours.' hours';
-			}else{
-				$hour_text = $durationHours.' hour';
-			}
+		if ($durationHours) {
+			$hour_text = $durationHours.' ';
+			$hour_text .= ($durationHours > 1) ? __('hours', 'tutor-pro') : __('hour', 'tutor-pro');
 		}
-		if ($durationMinutes){
-			if ($durationMinutes > 1){
-				$min_text = $durationMinutes.' minutes';
-			}else{
-				$min_text = $durationMinutes.' minute';
-			}
+		if ($durationMinutes) {
+			$min_text = $durationMinutes.' ';
+			$min_text .= ($durationMinutes > 1) ? __('minutes', 'tutor-pro') : __('minute', 'tutor-pro');
 		}
-		$duration_text= $hour_text.' '.$min_text;
+		$duration_text = $hour_text.' '.$min_text;
 		?>
         <h3>Certificate of completion</h3>
         <p>This is to certify that</p>
-        <h1><?php echo $user->display_name; ?></h1>
-        <p> has successfully completed <?php echo $duration_text; ?> the course</p>
+        <h1><?php echo $user->first_name, ' ', $user->last_name; ?></h1>
+        <p> has successfully completed the course</p>
         <h2><?php echo $course->post_title; ?></h2>
     </div>
 
     <div class="logo">
-        <img src="<?php echo $this->template['url'].'preview.png'; ?>" />
+        <img src="<?php echo $this->template['url'].'logo.png'; ?>" />
     </div>
 
     <div class="certificate-footer">
         <table>
             <tr>
                 <td class="first-col"><p>Wrocław, <?php echo date('jS F Y', strtotime( $completed->completion_date) ); ?></p></td>
+                <!-- <td class="first-col"><p>Wrocław, 1st October 2020</p></td> -->
                 <td class="last-col">
                     <div class="signature-wrap">
-						<?php
-						$signature_id = tutor_utils()->get_option('tutor_cert_signature_image_id');
-						$certURL = TUTOR_CERT()->url.'/assets/images/signature.png';
-						if ($signature_id){
-							$certURL = wp_get_attachment_url($signature_id);
-						}
-						?>
-                        <img src="<?php echo $certURL; ?>" />
+                        <img src="<?php echo $signature_image_url; ?>" />
                     </div>
                 </td>
             </tr>
@@ -68,7 +57,10 @@
             </tr>
             <tr>
                 <td class="first-col"><p><?php echo $completed->completed_hash; ?></p></td>
-                <!--td class="first-col"> <p>15/2020</p> </td-->
+                <!--td class="first-col"> <p>
+                    99/2020
+
+                </p> </td-->
                 <td class="last-col"><?php echo tutor_utils()->get_option('tutor_cert_authorised_company_name'); ?></td>
             </tr>
         </table>
